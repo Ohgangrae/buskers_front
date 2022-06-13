f58f88b9d6bb5c19dfe1506d56cce1b2390ceb40
Ohgangrae <oasisoyw@naver.com>
2022-06-13 15:09:47 +0900

    feat: third commit

--- slideShow2.js
+++ slideShow2.js
@@ -1,54 +1,49 @@
-let slides = document.querySelector('.slides');
-let slideImg = document.querySelectorAll('.slides li');
-currentIdx = 0;
-slideCount = slideImg.length;
-prev = document.querySelector('.prev'); //이전 버튼
-next = document.querySelector('.next'); //다음 버튼
-slideWidth = 300; //슬라이드이미지 넓이
-slideMargin = 100; //슬라이드 끼리의 마진값
-makeClone(); // 처음이미지와 마지막 이미지 복사 함수
-initfunction(); //슬라이드 넓이와 위치값 초기화 함수
-function makeClone() {
-  let cloneSlide_first = slideImg[0].cloneNode(true);
-  let cloneSlide_last = slides.lastElementChild.cloneNode(true);
-  slides.append(cloneSlide_first);
-  slides.insertBefore(cloneSlide_last, slides.firstElementChild);
-}
-function initfunction() {
-  slides.style.width = (slideWidth + slideMargin) * (slideCount + 2) + 'px';
-  slides.style.left = -(slideWidth + slideMargin) + 'px';
-}
-next.addEventListener('click', function () {
-  //다음 버튼 눌렀을때
-  if (currentIdx <= slideCount - 1) {
-    //슬라이드이동
-    slides.style.left = -(currentIdx + 2) * (slideWidth + slideMargin) + 'px';
-    slides.style.transition = `${0.5}s ease-out`; //이동 속도
-  }
-  if (currentIdx === slideCount - 1) {
-    //마지막 슬라이드 일때
-    setTimeout(function () {
-      //0.5초동안 복사한 첫번째 이미지에서, 진짜 첫번째 위치로 이동
-      slides.style.left = -(slideWidth + slideMargin) + 'px';
-      slides.style.transition = `${0}s ease-out`;
-    }, 500);
-    currentIdx = -1;
-  }
-  currentIdx += 1;
+//페이지번호와 슬라이드페이지 index연결
+$('.slider > .pages > div').click(function() {
+    var $this = $(this);
+    var $slider = $this.closest('.slider');
+    
+    $this.addClass('active');
+    $this.siblings('.active').removeClass('active');
+    
+    var index = $this.index();
+    
+    $slider.find(' > .slides > .active').removeClass('active');
+    $slider.find(' > .slides > div').eq(index).addClass('active');
 });
-prev.addEventListener('click', function () {
-  //이전 버튼 눌렀을때
-  console.log(currentIdx);
-  if (currentIdx >= 0) {
-    slides.style.left = -currentIdx * (slideWidth + slideMargin) + 'px';
-    slides.style.transition = `${0.5}s ease-out`;
-  }
-  if (currentIdx === 0) {
-    setTimeout(function () {
-      slides.style.left = -slideCount * (slideWidth + slideMargin) + 'px';
-      slides.style.transition = `${0}s ease-out`;
-    }, 500);
-    currentIdx = slideCount;
-  }
-  currentIdx -= 1;
+
+//버튼 클릭시 페이지번호 이동
+$('.slider > .side-btns > div').click(function() {
+    var $this = $(this);
+    var index = $this.index();
+    var $slider = $this.closest('.slider');
+    
+    var $current = $slider.find('.pages > div.active');
+    var $post;
+    
+    if ( index == 0 ) {
+        $post = $current.prev();
+    }
+    else {
+        $post = $current.next();
+    }
+    
+    if ( $post.length == 0 ) {
+        if ( index == 0 ) {
+            $post = $slider.find('.pages > div:last-child');
+        }
+        else {
+            $post = $slider.find('.pages > div:first-child');
+        }
+    }
+    
+    $post.click();
 });
+
+function Slider1__moveNext() {
+    var $slider = $('.slider-1');
+    var $nextBtn = $slider.find('.side-btns > div:last-child');
+    $nextBtn.click();
+}
+
+setInterval(Slider1__moveNext, 3000);
